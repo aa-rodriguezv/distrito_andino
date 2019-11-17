d2b0fb752ed67add553007cccecd0d41b0c25af3
aa_rodriguezv <aa.rodriguezv@uniandes.edu.co>
2019-11-17 15:06:32 -0500

    Correccion nombre

--- distrito_andino/lib/main.dart
+++ distrito_andino/lib/main.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('SAVE WOOD'),
+          title: Text('SAFE WOOD'),
         ),
         body: SingleChildScrollView(
                   child: Column(
